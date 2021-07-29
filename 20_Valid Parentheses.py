b4a14e5af2b710b75eaa6703d636cdfc5c561c20
Deepak Yadav <56887879+g0takh0R@users.noreply.github.com>
2021-07-29 16:17:54 +0530

    Update 20_Valid Parentheses.py

--- 20_Valid Parentheses.py
+++ 20_Valid Parentheses.py
@@ -1,3 +1,16 @@
+#This question can be solved by stack data structure
+"""
+Let's take a simple example : let the string be ([])
+So logic we'll use is, we will create a list name 'stack' in that whenever
+opening character comes (eg.'(','[','{') we will append. Also a dictonary will be created 
+in which closing char will be the key and opening char will be its values.Now we will append 
+those char to stack which are value and pop when dictionary key == value. So the first
+char is '(' so we will append it to stack and go to next char which is '[' so now this is appended 
+in stack. Next char comes ']' now we will use this char as key to get value of it and compare
+with the poped value of stack if same we'll continue or return false. Like this at last if we get 
+an empty stack then we return true.
+"""
+
 class Solution:
     def isValid(self, s: str) -> bool:
         dic={')':'(','}':'{',']':'['}
@@ -11,3 +24,9 @@ class Solution:
             else:
                 return False       
         return stack==[]
+#to check any test cases uncomment and change value of s
+"""
+s = "([])"
+obj=Solution
+print(Solution().isValid(s))
+"""
